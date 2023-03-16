8c1229f163317f421ddf28ff82600a7aa9b5ea87
kcryns <rayneskarlcydrick@gmail.com>
2023-03-16 15:46:33 +0800

    Spotify-Premium

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,18 @@
 const toggleMenu = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav ul");
+const arjay = document.querySelector(".arjay");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerNav.classList.toggle("open");
   headerNav.style=("transition: 300ms");
+  arjay.classList.toggle("open");
 });
+
+const kamote = document.querySelectorAll('.title');
+kamote.forEach((kamoteq) => {
+    kamoteq.addEventListener('click', () => {
+        kamoteq.nextElementSibling.classList.toggle('open');
+        kamoteq.querySelector("i").classList.toggle("open");
+       
+    })
+})
