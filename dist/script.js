c937065b01700c45465a8fc60ede9a9f456a93a5
kcryns <rayneskarlcydrick@gmail.com>
2023-03-20 15:42:52 +0800

    tailwind-spotify

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,12 @@
+const toggleMenu2 = document.querySelector(".toggle__menu2");
+const headerNav2 = document.querySelector(".navigation ul");
+toggleMenu2.addEventListener("click", () => {
+  toggleMenu2.classList.toggle("open");
+  headerNav2.classList.toggle("open");
+  headerNav2.style=("transition: 300ms");
+});
+
+
 const toggleMenu = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav ul");
 const arjay = document.querySelector(".arjay");
@@ -7,6 +16,8 @@ toggleMenu.addEventListener("click", () => {
   headerNav.style=("transition: 300ms");
   arjay.classList.toggle("open");
 });
+
+
 
 const kamote = document.querySelectorAll('.drop__item h4');
 kamote.forEach((kamoteq) => {
