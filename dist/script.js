c32aeabd887370450421410ddc043eabaedd6554
kcryns <rayneskarlcydrick@gmail.com>
2023-03-21 15:11:50 +0800

    latest

--- dist/script.js
+++ dist/script.js
@@ -1,11 +1,23 @@
 const toggleMenu2 = document.querySelector(".toggle__menu2");
 const headerNav2 = document.querySelector(".navigation ul");
+const xsearch = document.querySelector(".search__icon");
+const hatdog = document.querySelector(".hatdog");
+const ekis = document.querySelector(".ekis");
 toggleMenu2.addEventListener("click", () => {
   toggleMenu2.classList.toggle("open");
   headerNav2.classList.toggle("open");
   headerNav2.style=("transition: 300ms");
 });
 
+xsearch.addEventListener("click", () => {
+  hatdog.classList.toggle("open");
+
+});
+
+ekis.addEventListener("click", () => {
+  hatdog.classList.toggle("open");
+
+});
 
 const toggleMenu = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav ul");
