96a36c63990809759e17ea0232794aea65f8a683
kcryns <rayneskarlcydrick@gmail.com>
2023-04-27 15:41:46 +0800

    Web-Portfolio

--- dist/script.js
+++ dist/script.js
@@ -1,41 +1,6 @@
-const toggleMenu2 = document.querySelector(".toggle__menu2");
-const headerNav2 = document.querySelector(".navigation ul");
-const xsearch = document.querySelector(".search__icon");
-const hatdog = document.querySelector(".hatdog");
-const ekis = document.querySelector(".ekis");
-toggleMenu2.addEventListener("click", () => {
-  toggleMenu2.classList.toggle("open");
-  headerNav2.classList.toggle("open");
-  headerNav2.style=("transition: 300ms");
-});
-
-xsearch.addEventListener("click", () => {
-  hatdog.classList.toggle("open");
-
-});
-
-ekis.addEventListener("click", () => {
-  hatdog.classList.toggle("open");
-
-});
-
 const toggleMenu = document.querySelector(".toggle__menu");
-const headerNav = document.querySelector(".header__nav ul");
-const arjay = document.querySelector(".arjay");
+const headerNav = document.querySelector(".navigation");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerNav.classList.toggle("open");
-  headerNav.style=("transition: 300ms");
-  arjay.classList.toggle("open");
 });
-
-
-
-const kamote = document.querySelectorAll('.drop__item h4');
-kamote.forEach((kamoteq) => {
-    kamoteq.addEventListener('click', () => {
-        kamoteq.nextElementSibling.classList.toggle('open');
-        kamoteq.querySelector("i").classList.toggle("open");
-       
-    })
-})
